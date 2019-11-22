c3db8b7be0db5879f3c5af4b3b49451da35269af
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-11-21 23:22:26 -0500

    added logic to get best match, trying to populate to modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,18 +9,33 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
 
-        
+        console.log(req.body.scores)
 
+        var newDirectors = req.body.scores;
+        var scoresArr = [];
+        var match = 0;
+
+        for (var i = 0; i < directorsScores.length; i++) {
+                matchDifference = 0;
+            for (var j = 0; i < newDirectors.length; j++) {
+                matchDifference += (Math.abs(parseInt(directorsScores[i].scores[j]) - parseInt(newDirectors[j])));
+            }
+
+            scoresArr.push(matchDifference);
+
+            
+        }
+        for (var i = 0; i < scoresArr.length; i++){   
+            if(scoresArr[i] <= scoresArr[match]) {
+                match = i
+            }
+            
+        }
+        res.send(directorsScores[match]);
+        directorsScores.push(req.body);
+
+    
     });
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.use(express.static(__dirname + './app/public'));
+app.use(express.static(__dirname + '/app/public'));
 
 
 app.listen(PORT, function(){
