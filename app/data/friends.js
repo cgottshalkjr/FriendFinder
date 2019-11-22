7bd03ee9012173b5d224755778ebfc5aee76efa7
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-11-21 19:08:45 -0500

    Fixing html/css. Starting routes pages. Trying to fix bug on server.js

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var directors = [
+    {
+        name: "Andrei Tarkovsky",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/b/b5/Andrei_Tarkovsky.jpg",
+        scores: [5, 1, 1, 3, 5, 4, 4, 2, 3, 4]
+    }, {
+        name: "Werner Herzog",
+        photo: "https://pixel.nymag.com/imgs/daily/vulture/2015/05/21/21-werner-herzog.w700.h700.jpg",
+        scores: [3, 2, 1, 3, 4, 1, 4, 1, 2, 5]
+    }, {
+        name: "Ingmar Bergman",
+        photo: "https://www.dga.org/-/media/Images/DGAQ-Article-Images/DGA-Quarterly/1201-Winter-2012/profile-ingmar-bergman-1.ashx?la=en&hash=4FFD49EE5CC474C72DF63D693676B00B8DBAF4FF",
+        scores: [4, 4, 4, 5, 5, 5, 2, 4, 5, 5]
+    }, {
+        name: "Akira Kurosawa",
+        photo: "https://8hours-prod.s3.amazonaws.com/uploads/2018/08/AkiraKurosawa2-427x640.jpg",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    }, {
+        name: "Martin Scorsese",
+        photo: "https://i.redd.it/gnln6la264311.jpg",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    }, {
+        name: "Robert Bresson",
+        photo: "https://www.bookforum.com/uploads/upload.000/id16823/article00_large.jpg",
+        scores: [1, 4, 1, 3, 2, 2, 2, 5, 5, 4]
+    }, {
+        name: "Carl Th. Dreyer",
+        photo: "https://s3.amazonaws.com/criterion-production/explore_images/451-3baa81d08f1127d076676df120716844/kUXxToH2e8z6QDlwBPHgbDu9kdL81x_original.jpg",
+        scores: [1, 4, 5, 5, 2, 5, 2, 1, 2, 4]
+    }, {
+        name: "Seijun Suzuki",
+        photo: "https://www.bfi.org.uk/sites/bfi.org.uk/files/styles/full/public/image/suzuki-seijun-002-hands-wide-in-front-of-house-crop.jpg?itok=kdXqo0vm",
+        scores: [2, 5, 1, 3, 3, 3, 4, 4, 5, 2]
+    }
+]
+
+module.exports = directors;
--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
 
 var app = express()
 var PORT = process.env.PORT || 8080;
 
-app.get("/", function (req, res){
-    res.send("Hello World!");
-})
- 
-app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(bodyParser.json())
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function(){
