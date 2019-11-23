5ce64a0d4577af431beded01213c06a538374d60
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-11-22 22:52:17 -0500

    Adding some director info, Adding css and styling.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,36 +2,43 @@ var directors = [
     {
         name: "Andrei Tarkovsky",
         photo: "https://upload.wikimedia.org/wikipedia/commons/b/b5/Andrei_Tarkovsky.jpg",
-        scores: [5, 1, 1, 3, 5, 4, 4, 2, 3, 4]
+        scores: [5, 1, 1, 3, 5, 4, 4, 2, 3, 4],
+        films: "Director of such fine films as Stalker, Andrei Rublev, Solaris, The Mirror, Nostalgia, and more!"
     }, {
         name: "Werner Herzog",
         photo: "https://pixel.nymag.com/imgs/daily/vulture/2015/05/21/21-werner-herzog.w700.h700.jpg",
-        scores: [3, 2, 1, 3, 4, 1, 4, 1, 2, 5]
+        scores: [3, 2, 1, 3, 4, 1, 4, 1, 2, 5],
+        films: "Director of such fine films as Aguirre The Wrath of God, Stroszek, Nosferatu, Fitzcarraldo, and more!"
     }, {
         name: "Ingmar Bergman",
         photo: "https://www.dga.org/-/media/Images/DGAQ-Article-Images/DGA-Quarterly/1201-Winter-2012/profile-ingmar-bergman-1.ashx?la=en&hash=4FFD49EE5CC474C72DF63D693676B00B8DBAF4FF",
         scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-        link: "https://www.criterion.com/boxsets/1427-ingmar-bergman-s-cinema"
+        films: "Director of such fine films as Persona, The Seventh Seal, Winter Light, and more!"
     }, {
         name: "Akira Kurosawa",
         photo: "https://8hours-prod.s3.amazonaws.com/uploads/2018/08/AkiraKurosawa2-427x640.jpg",
-        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+        films: "Director of such fine films as Seven Samurai, Rashomon, High and Low, Ikiru, Yojimbo, and more!"
     }, {
         name: "Martin Scorsese",
         photo: "https://i.redd.it/gnln6la264311.jpg",
-        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        films: "Director of such fine films as Taxi Driver, The King of Comedy, Raging Bull, Goodfellas, After Hours, and more!"
     }, {
         name: "Robert Bresson",
         photo: "https://www.bookforum.com/uploads/upload.000/id16823/article00_large.jpg",
-        scores: [1, 4, 1, 3, 2, 2, 2, 5, 5, 4]
+        scores: [1, 4, 1, 3, 2, 2, 2, 5, 5, 4],
+        films: "Director of such fine films as Diary of a Country Priest, Pickpocket, A Man Escaped, Au Hasard Balthazar, L'Argent, and more!"
     }, {
         name: "Carl Th. Dreyer",
         photo: "https://s3.amazonaws.com/criterion-production/explore_images/451-3baa81d08f1127d076676df120716844/kUXxToH2e8z6QDlwBPHgbDu9kdL81x_original.jpg",
-        scores: [1, 4, 5, 5, 2, 5, 2, 1, 2, 4]
+        scores: [1, 4, 5, 5, 2, 5, 2, 1, 2, 4],
+        films: "Director of such fine films as Vampyr, Ordet, The Passion of Joan of Arc, Day of Wrath, and more!"
     }, {
         name: "Seijun Suzuki",
         photo: "https://www.bfi.org.uk/sites/bfi.org.uk/files/styles/full/public/image/suzuki-seijun-002-hands-wide-in-front-of-house-crop.jpg?itok=kdXqo0vm",
-        scores: [2, 5, 1, 3, 3, 3, 4, 4, 5, 2]
+        scores: [2, 5, 1, 3, 3, 3, 4, 4, 5, 2],
+        films: "Director of such fine films as Tokyo Drifter Branded to Kill, There's a Bird Inside a Man, and more!"
     }
 ]
 
